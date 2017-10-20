37976c2af299b02c40a4f18910fc048fca360f7a
56 <s0936929599@yahoo.com.tw>
2017-10-20 16:52:02 +0800

    Update air_condition.py

--- air_condition.py
+++ air_condition.py
@@ -3,6 +3,19 @@ import numpy as np
 import pandas as pd
 import glob
 import re
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import normalize
 os.getcwd() #dir
 #preprocess
 path =r'C:\Users\huang\Desktop\train'
@@ -47,3 +60,29 @@ for i in range(3,len(a)):
 b=b.drop('平均',axis=1)
 b=b.drop(b.index[0:3])
 b
+
+#model lstm
+end=np.around(b.values[:,3:].astype("float32"))
+end=end.astype('int').copy()
+#scaler = MinMaxScaler(feature_range=(0, 1))
+#end = scaler.fit_transform(end)
+end=normalize(end)
+end
+train_X, train_y = end[:3000, :-1], end[:3000,-1]
+test_X, test_y = end[3000:, :-1], end[3000:,-1]
+train_X,test_X
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0],1,train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0],1,test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+model = Sequential()
+model.add(LSTM(10, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+
+history=model.fit(train_X, train_y, epochs=50, batch_size=30,validation_data=(test_X, test_y),verbose=2, shuffle=False)
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
